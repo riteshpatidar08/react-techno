4357e0e5db214c6c3379fe1ad0edd69d3fb1e7bb
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-08-30 10:16:12 +0530

    updated till useEffect

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,16 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect ,useState } from 'react';
+import Card from './Card';
 
 function Product() {
+    const [product , setProduct] = useState([]) ;
+
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch('https://dummyjson.com/products');
       const data = await res.json();
-      console.log(data.products);
+      setProduct(data.products);
     };
     fetchData();
   }, []);
-  
-  return <div></div>;
+
+  return <div>
+    {product.map((el)=>(
+        <Card img={el.images[0]} text={el.title} />
+    ))}
+  </div>;
 }
 
 export default Product;
