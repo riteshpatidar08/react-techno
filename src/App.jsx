7c943a8de67ace9b132c3157ac3ce59537289f3c
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-08-31 10:07:15 +0530

    dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Product from './components/Product'
+import { Routes, Route } from 'react-router-dom'
 function App() {
   return (
-    <div>
-      <Product/>
-    </div>
+   <Routes>
+    <Route path='/product' element= {<Product/>}/>
+   </Routes>
   )
 }
 
 export default App
+
+//NOTE import BrowserRouter component in the main.jsx file and wrap the <App/> component in it
+//NOTE Go to the App.jsx file import the Routes , Route component 
+//NOTE call the Routes at top level then render Route component for each route you have to create it accept the path and element prop in path you have to pass the path where the component is going to show and in the element you have to pass which component is going to show on the screen.
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { DataProvider } from './context/Datacontext.jsx'
-
+import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter>
     <DataProvider>
       <App/> 
     </DataProvider>
+    </BrowserRouter>
   
   </StrictMode>,
 )
